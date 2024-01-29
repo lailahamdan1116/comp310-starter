ef17dccd014a0ce358a330c581c8bd80054506c5
laila hamdan <lailahamdan786@gmail.com>
2024-01-29 00:08:31 +0000

    commit

---
+++ Warmup_9/fileoperations.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char *argv[]){
+    FILE *file;
+    char filename[] = "example.txt";
+    char line[100];
+
+    file = fopen(filename, w);
+
+    if(file == NULL){
+        printf("Error!");
+        exit (1);
+    }
+
+    fprintf(file, "Hello, World!");
+    fclose(file);
+    return 0;
+
+    if((file == fopen(filename, r))== NULL){
+        printf("Error!");
+        exit(1);
+    }
+    while(fgets(line, 100, file)){
+        printf("%s", line);
+    }
+    fclose(file);
+
+    
+}
