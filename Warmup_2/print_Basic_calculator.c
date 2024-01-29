8034bbe216b9344d64ea10e504ba12fb043d7c1a
laila hamdan <lailahamdan786@gmail.com>
2024-01-29 05:02:01 +0000

    commit

--- Warmup_2/print_Basic_calculator.c
+++ Warmup_2/print_Basic_calculator.c
@@ -10,9 +10,9 @@ int main(){
     printf("Enter second number: \n");
     scanf("%lf", &num2);
 
-    printf("Sum: %lf", num1 + num2);
-    printf("Difference: %lf", num1 - num2);
-    printf("Quotient: %lf", num1/num2);
-    printf("Product: %lf", num1 * num2);
+    printf("Sum: %lf \n", num1 + num2);
+    printf("Difference: %lf \n", num1 - num2);
+    printf("Quotient: %lf \n", num1/num2);
+    printf("Product: %lf \n", num1 * num2);
 
 }
